fbc774f73bf239c2aa89d5cc1bf198864aa91fa2
budiyono-dev <budiyono.dev@gmail.com>
2023-06-06 20:57:13 +0700

    add boostrap

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return 'this is home route';
+});
+
+Route::get('/navbar', function () {
+    return view('navbar');
+});
+
+Route::get('/get-data', function () {
+    return 'ini data yang di return';
+});
