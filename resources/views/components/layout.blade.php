4ffc9b1f2cc962fbf6a2827ddc6fe7e9a96ae1c2
budiyono-dev <budiyono.dev@gmail.com>
2023-06-08 22:22:27 +0700

    update

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,14 +1,17 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
     <style>
     </style>
-  </head>
-  <body>
+</head>
+
+<body>
     {{ $slot }}
-  </body>
+</body>
+
 </html>
