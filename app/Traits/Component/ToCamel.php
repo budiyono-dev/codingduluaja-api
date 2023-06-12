1534f72ae511b58a9e3e0e4207c2c7e9488c7c11
budiyono-dev <budiyono.dev@gmail.com>
2023-06-12 22:32:29 +0700

    delete comment

--- app/Traits/Component/ToCamel.php
+++ app/Traits/Component/ToCamel.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Str;
 
 trait ToCamel
 {
-    // Trait methods and properties go here
     public function toCamel(string $text) : string
     {
         return Str::camel($text);
--- app/View/Components/Layout/Main.php
+++ app/View/Components/Layout/Main.php
@@ -8,20 +8,14 @@ use Illuminate\View\Component;
 
 class Main extends Component
 {
-    /**
-     * Create a new component instance.
-     */
     public function __construct
     (
         public string $title
     )
     {
-        @dd(request());
+        // @dd(request());
     }
 
-    /**
-     * Get the view / contents that represent the component.
-     */
     public function render(): View|Closure|string
     {
         return view('components.layout.main');
