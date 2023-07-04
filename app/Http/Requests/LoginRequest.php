ac73a753da762f99286c58065507b5d410963c6c
budiyono-dev <budiyono.dev@gmail.com>
2023-07-04 16:36:50 +0700

    slimify request

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -19,31 +19,4 @@ class LoginRequest extends FormRequest
             'password' => 'required'
         ];
     }
-
-    public function attributes(): array
-    {
-        return [
-            'email' => 'Email',
-            'password' => 'Password',
-        ];
-    }
-
-//    public function messages(): array
-//    {
-//        return [
-//            'first_name.required' => __('validation.required'),
-//            'first_name.min' => __('validation.min'),
-//            'first_name.max' => __('validation.max'),
-//            'last_name.min' => __('validation.min'),
-//            'last_name.max' => __('validation.max'),
-//            'email.required' => __('validation.required'),
-//            'email.email' => __('validation.email'),
-//            'email.unique' => __('validation.unique'),
-//            'email.max' => __('validation.max'),
-//            'sex.in' => __('validation.in'),
-//            'password.required' => __('validation.required'),
-//            'password.min' => __('validation.min')
-//
-//        ];
-//    }
 }
