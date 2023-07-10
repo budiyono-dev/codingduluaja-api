edac1f64fac8d8589b030de19a3414b196274882
budiyono-dev <budiyono.dev@gmail.com>
2023-07-10 18:34:39 +0700

    save progress

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -14,28 +14,7 @@ class UserTest extends TestCase
      */
     public function test_example(): void
     {
-        // dd('jalankan test');
-            //         $table->id();
-            // $table->string('username')->unique();
-            // $table->string('firstname');
-            // $table->string('lastname');
-            // $table->string('sex');
-            // $table->string('email')->unique();
-            // $table->string('password');
-            // $table->timestamp('email_verified_at')->nullable();
-        // $user = User::create([
-        //     'username' => 'myusername',
-        //     'firstname' => 'budiyono',
-        //     'email'=>'budi@mail.com'
-        // ]);
-        $user = new User();
-        $user->username = 'myusername';
-        $user->firstname = 'firstname';
-        $user->email = 'budi@maiil.com';
-        $user->password = 'mypassword';
-        $user->save();
-
-        $data = User::all()->toArray();
-        dd($data);
+        $end = env('MYCONF', 'not-found');
+        dd("endndn".$end);
     }
 }
