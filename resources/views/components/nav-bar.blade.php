786ab1b4178d2ee0f1515b8178302b0b8c574e8d
budiyono-dev <budiyono.dev@gmail.com>
2023-08-01 13:39:07 +0700

    composer update

--- resources/views/components/nav-bar.blade.php
+++ resources/views/components/nav-bar.blade.php
@@ -8,15 +8,6 @@
             </svg>
         </div>
         <a class="navbar-brand" href="{{ route('page.dashboard') }}">Codingduluaja</a>
-        {{-- <button class="btn btn-secondary" type="button" aria-label="Toggle Sidegbar" id="btn-toggle-sidebar">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-grid-fill" viewBox="0 0 16 16">
-        <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zm8 0A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm-8 8A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm8 0A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3z"/>
-      </svg> --}}
-        </button>
-        {{-- <button class="navbar-toggler " type="button" aria-label="Toggle Sidegbar" id="btn-toggle-sidebar">
-      <span class="navbar-toggler-icon btn-sm"></span>
-    </button>
-    <span class="navbar-brand mb-0 h1 mx-auto">Title</span> --}}
         <div class="d-flex align-items-center">
             <form class="d-flex" action="{{ route('do.logout') }}" method="POST">
                 @csrf
