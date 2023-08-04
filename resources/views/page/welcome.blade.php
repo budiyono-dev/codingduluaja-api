9ef1936621f29158fa38eee1154784feca302cb5
budiyono-dev <budiyono.dev@gmail.com>
2023-08-04 18:04:40 +0700

    enhance welcome blade

--- resources/views/page/welcome.blade.php
+++ resources/views/page/welcome.blade.php
@@ -1,44 +1,26 @@
 <x-layout.main-sidebar title="Home">
-  {{-- <div id="main" class="main-content">
-    <div class="row">
-      <div class="col text-center border border-dark">
-        <x-nav-bar />
-      </div>
-    </div>
-    <div class="row">
-      <div class="col text-center">
-        column 2
-      </div>
-    </div>
-  </div> --}}
-  Add Resource
-  
-  <button type="button" class="btn btn-primary btn-lg" id="btnCreateToken">
-    Create Token
-  </button>
+    Add Resource
 
-@push('script')
-<script>
-// <form action="{{ route('do.createToken') }}">
-const createToken = async (el) => {
-try {
-    const res = await fetch("{{ route('do.createToken') }}");
-    console.log(res);
-} catch (e) {
-    log.error('error', e);
-}
-    console.log(el);
-    console.log(el.target);
-}
+    <button type="button" class="btn btn-primary btn-lg" id="btnCreateToken">
+        Create Token
+    </button>
 
-
-</script>
-@endpush
-@push('addEventListener')
-    <script type="text/javascript">
-        document.getElementById('btnCreateToken').addEventListener('click', createToken);
-    </script>
-@endpush
+    @push('script')
+        <script>
+            const createToken = async (el) => {
+                try {
+                    const res = await fetch("{{ route('do.createToken') }}");
+                    const jsonRes = await res.json();
+                    console.log(jsonRes);
+                } catch (e) {
+                    log.error('error', e);
+                }
+            }
+        </script>
+    @endpush
+    @push('addEventListener')
+        <script type="text/javascript">
+            document.getElementById('btnCreateToken').addEventListener('click', createToken);
+        </script>
+    @endpush
 </x-layout.main-sidebar>
-
-
