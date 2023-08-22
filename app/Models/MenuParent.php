a936fadbc84984d9d283189bc1d53ef3942ec864
budiyono-dev <budiyono.dev@gmail.com>
2023-08-22 17:45:00 +0700

    modifying migration

--- app/Models/MenuParent.php
+++ app/Models/MenuParent.php
@@ -5,8 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ExpiredAppToken extends Model
+class MenuParent extends Model
 {
     use HasFactory;
-    protected $table = 'expired_app_token';
+    protected $table = 'menu_parent';
 }
---
+++ database/migrations/2023_08_18_171707_create_menu_item_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('menu_item', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBiginteger('menu_parent_id');
+            $table->string('name', 50);
+            $table->string('page', 30);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('menu_item');
+    }
+};
