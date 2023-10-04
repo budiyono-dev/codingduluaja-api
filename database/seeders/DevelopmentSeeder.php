35992475f8a7bb2abadc502c1fea86a116bcd1ca
budiyono-dev <budiyono.dev@gmail.com>
2023-10-04 20:39:42 +0700

    rename file

--- database/seeders/DevelopmentSeeder.php
+++ database/seeders/DevelopmentSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class DevelopmentSeeeder extends Seeder
+class DevelopmentSeeder extends Seeder
 {
     public function run(): void
     {
