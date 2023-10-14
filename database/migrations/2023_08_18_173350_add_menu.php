b6408348df98a65062837db7322a8cc5b544cdf3
budiyono-dev <budiyono.dev@gmail.com>
2023-10-14 08:08:43 +0700

    migration utils

--- database/migrations/2023_08_18_173350_add_menu.php
+++ database/migrations/2023_08_18_173350_add_menu.php
@@ -4,60 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 use App\Models\MenuParent;
 use App\Models\MenuItem;
+use App\Helper\MigrationUtils;
 
 return new class extends Migration
 {
 
     public function up(): void
     {
-        $menuApplication = new MenuParent(
-            [
-                'name' => 'menu.parent.application',
-                'sequence' => 1
-            ]
-        );
+        $menuApplication = MigrationUtils::insertMenuParent('menu.parent.application', 1);
 
-        $menuApplication->save();
-
-        $itemAppResource = new MenuItem(
-            [
-                'menu_parent_id' => $menuApplication->id,
-                'name' => 'menu.item.app_resource',
-                'page' => 'page.appResource',
-                'sequence' => 1
-            ]
-        );
-
-        $itemAppResource->save();
-
-        $itemClientApp = new MenuItem(
-            [
-                'menu_parent_id' => $menuApplication->id,
-                'name' => 'menu.item.client_app',
-                'page' => 'page.clientApp',
-                'sequence' => 2
-            ]
-        );
-
-        $itemClientApp->save();
-
-        $itemAppManager = new MenuItem(
-            [
-                'menu_parent_id' => $menuApplication->id,
-                'name' => 'menu.item.app_manager',
-                'page' => 'page.appManager',
-                'sequence' => 3
-            ]
-        );
-
-        $itemAppManager->save();
+        MigrationUtils::insertMenuItem($menuApplication->id, 'menu.item.app_resource', 'page.appResource', 1);
+        MigrationUtils::insertMenuItem($menuApplication->id, 'menu.item.client_app', 'page.clientApp', 2);
+        MigrationUtils::insertMenuItem($menuApplication->id, 'menu.item.app_manager', 'page.appManager', 3);
+        
+        
     }
 
     public function down(): void
     {
-        $menu1 = MenuParent::where('sequence', 1)->first();
-        
-        $subMenus = MenuItem::where('menu_parent_id', $menu1->id);
-        $subMenus->delete();
+        MigrationUtils::deleteMenuParent(1);
     }
 };
