aadf216723cb70d7944bf3596a2dedeae848d162
DESKTOP-M67BEJQ\budi9 <budiyono.dev@gmail.com>
2024-03-13 23:47:29 +0700

    [API-005] mockup landing page

--- resources/views/page/landing-page.blade.php
+++ resources/views/page/landing-page.blade.php
@@ -5,8 +5,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/lp.css') }}">
 </head>
 <body>
-    wellcome to landingpage
+    <header>
+        <div class="brand">
+            Codingduluaja
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#about">About</a></li>
+                <li><a href="#api">API</a></li>
+                <li><a href="#doc">Documentation</a></li>
+                <li><a href="#donate">Donate</a></li>
+                <li><a href="#faq">FAQ</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section id="about">
+            prolog
+        </section>
+        <section id="api">
+            api
+        </section>
+        <section id="doc">
+            doc
+        </section>
+        <section id="faq">
+            faq
+        </section>
+    </main>
+    <footer>
+        Footer
+    </footer>
 </body>
 </html>
