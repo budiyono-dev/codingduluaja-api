e145aa776228e86e7a4b20a0b6034b8c33042984
budiyono-dev <budiyono.dev@gmail.com>
2024-03-23 16:17:33 +0700

    [API-005] add icon

--- resources/views/page/landing-page.blade.php
+++ resources/views/page/landing-page.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,26 +8,39 @@
     <title>Document</title>
     <link rel="stylesheet" href="{{ asset('assets/css/lp.css') }}">
 </head>
+
 <body>
     <header>
         <div class="brand">
             Codingduluaja
         </div>
-        <nav>
-            <ul>
-                <li><a href="#about">About</a></li>
-                <li><a href="#api">API</a></li>
-                <li><a href="#doc">Documentation</a></li>
-                <li><a href="#donate">Donate</a></li>
-                <li><a href="#faq">FAQ</a></li>
-            </ul>
-        </nav>
+
+        <div class="nav">
+            <nav>
+                <ul>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#api">API</a></li>
+                    <li><a href="#doc">Documentation</a></li>
+                    <li><a href="#donate">Donate</a></li>
+                    <li><a href="#faq">FAQ</a></li>
+                </ul>
+            </nav>
+            <button id="toggleDark" type="button" class="dark-mode dark" >
+                <x-icon.fa-sun></x-icon.fa-sun>
+            </button>
+            <button id="toggleLight" type="button" class="dark-mode dark" style="display: none">
+                <x-icon.fa-sun-reg></x-icon.fa-sun-reg>
+            </button>
+        </div>
+
     </header>
     <main>
         <section id="about" class="page">
             <h2>Codingduluaja API</h2>
-            <p>coding dulu aja pahamnya nanti</p>
-            <button type="button">Documentation</button>
+            <p>-- coding dulu aja pahamnya nanti --</p>
+            <button type="button" class="btn-doc">
+                Documentation
+            </button>
         </section>
         <section id="api" class="page">
             api
@@ -41,5 +55,36 @@
     <footer>
         Footer
     </footer>
+    <script>
+        let lpTm = localStorage.getItem("lp-theme");
+        function switchPallete(){
+            const r = document.querySelector(':root');
+            r.classList.toggle('dark');
+
+        }
+        function changeTheme(theme) {
+            console.log('chane', theme);
+            if (theme == 'dark') {
+                document.getElementById('toggleDark').style.display = 'none';
+                document.getElementById('toggleLight').style.display = 'block';
+            } else {
+                document.getElementById('toggleDark').style.display = 'block';
+                document.getElementById('toggleLight').style.display = 'none';
+            }
+            localStorage.setItem('lp-theme', theme);
+            switchPallete();
+        }
+
+        if (lpTm) {
+            const r = document.querySelector(':root');
+            const current = r.classList.contains('darl') ? 'dark' : 'light';
+            if (current != lpTm) { 
+                changeTheme(lpTm)
+            }
+        }
+        document.getElementById('toggleDark').addEventListener('click', () => changeTheme('dark'));
+        document.getElementById('toggleLight').addEventListener('click', () => changeTheme('light'));
+    </script>
 </body>
+
 </html>
