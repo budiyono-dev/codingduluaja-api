a5e0a4a3cd1078cca63ba53cab9f13aa6ffae192
budiyono-dev <budiyono.dev@gmail.com>
2024-07-08 21:23:36 +0700

    [API-015] gabung create role and user

--- database/migrations/2024_06_24_000003_add_user_and_role.php
+++ database/migrations/2024_06_24_000003_add_user_and_role.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Constants\UserRole;
 use App\Helper\MigrationUtils;
 use Illuminate\Database\Migrations\Migration;
 
@@ -7,6 +8,16 @@ return new class extends Migration
 {
     public function up(): void
     {
+        $admin = UserRole::admin();
+        $su = UserRole::superUser();
+        $ops = UserRole::ops();
+        $user = UserRole::user();
+
+        MigrationUtils::addUserRole($admin->getCode(), $admin->getName());
+        MigrationUtils::addUserRole($su->getCode(), $su->getName());
+        MigrationUtils::addUserRole($ops->getCode(), $ops->getName());
+        MigrationUtils::addUserRole($user->getCode(), $user->getName());
+        
         MigrationUtils::addUserAdmin('Admin Satu', 'admin1', 'admin1@codingduluaja.com');
         MigrationUtils::addUserUser('Tester Satu', 'tester1', 'tester1@codingduluaja.com');
         MigrationUtils::addUserSu('Admin Super', 'adminsu', 'adminsu1@codingduluaja.com');
