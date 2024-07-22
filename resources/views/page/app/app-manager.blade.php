0d2441f6f39085b33ea366fb0d5f143066726e83
budiyono-dev <budiyono.dev@gmail.com>
2024-07-22 22:29:59 +0700

    [API-015] change button

--- resources/views/page/app/app-manager.blade.php
+++ resources/views/page/app/app-manager.blade.php
@@ -12,19 +12,25 @@
                             {{ $res->masterResource->name }}
                         </a>
                         <ul class="list-group collapse" id="{{ 'collapse' . $key }}">
+                            <li class="list-group-item list-group-item-warning">
+                                <a type="button" class="btn btn-success btn-sm" 
+                                   href="{{route('page.app.connectManager', ['resourceId'=>$res->id])}}">Connect the Client</a>
+                            </li>
                             @forelse ($res->connectedApp as $ca)
                                 <li class="list-group-item list-group-item-warning">
                                     <p>{{ $ca->name }}</p>
                                     <div class="d-flex gap-3 my-2">
-                                    <button type="button" class="btn btn-primary btn-sm">Craete Token</button>
-                                    <button type="button" class="btn btn-primary btn-sm">Revoke Token</button>
+                                    <button type="button" class="btn btn-primary btn-sm">Create Token</button>
+                                    <button type="button" class="btn btn-danger btn-sm">Revoke Token</button>
                                     </div>
                                 </li>
                             @empty
-                                <p>No Data....</p>
+                            <li class="list-group-item list-group-item-warning">
+                                <span>No Data....</span>
+                            </li>
                             @endforelse
-                            <a type="button" class="btn btn-success btn-sm" 
-                               href="{{route('page.app.connectManager', ['resourceId'=>$res->id])}}">Connect the Client</a>
+                            {{-- <a type="button" class="btn btn-success btn-sm" 
+                               href="{{route('page.app.connectManager', ['resourceId'=>$res->id])}}">Connect the Client</a> --}}
                         </ul>
                     @empty
                         <p>No Data....</p>
