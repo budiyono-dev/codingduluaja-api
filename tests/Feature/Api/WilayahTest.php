7fd6dc1a8300d036f6dfb2f9e970a703e81a93a4
budiyono-dev <budiyono.dev@gmail.com>
2024-07-31 18:03:27 +0700

    [API-015] wilayah test

--- tests/Feature/Api/WilayahTest.php
+++ tests/Feature/Api/WilayahTest.php
@@ -6,6 +6,11 @@ use Tests\TestCase;
 
 class WilayahTest extends TestCase
 {
+    private static $tokenWilayah = null;
+    public static function setUpBeforeClass(): void
+    {
+        
+    }
     public function test_login_screen_can_be_rendered(): void
     {
         $response = $this->get('/api/todolist');
