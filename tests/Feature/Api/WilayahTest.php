22ec7e7cbd806187b289e56e2430d3079327ab37
budiyono-dev <budiyono.dev@gmail.com>
2024-07-31 16:34:23 +0700

    [API-015] wilayah test

---
+++ tests/Feature/Api/WilayahTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use Tests\TestCase;
+
+class TodolistTest extends TestCase
+{
+    public function test_login_screen_can_be_rendered(): void
+    {
+        $response = $this->get('/api/todolist');
+
+        $response->dump();
+        $response->assertStatus(200);
+    }
+}
