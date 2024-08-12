cbcea017fd706e74f6267bf8c4d2c53b8ce36c00
budiyono-dev <budiyono.dev@gmail.com>
2024-08-12 15:40:19 +0700

    [API-021] up version

--- config/app.php
+++ config/app.php
@@ -130,7 +130,7 @@ return [
     'init_su_url' => (string) env('INIT_SU_URL', 'VzNPBNLaNdRGyrrhsKtnLOctOTamPaEu'),
     'seeder_user_api_qty' => (int) env('SEEDER_USER_API_QTY', 5),
     'page_size' => (int) env('PAGINATION_PAGE_SIZE', 5),
-    'app_version' => (string) env('APP_VERSION', '2.0.1'),
+    'app_version' => (string) env('APP_VERSION', '2.0.2'),
     'default_password' => (string) env('DEFAULT_PASSWORD', 'Admin#2024'),
 
 ];
