be2944cec6f43144a4d6f7dccd562aa5e84d1857
budiyono-dev <budiyono.dev@gmail.com>
2024-09-07 15:21:50 +0700

    [API-035] migration

--- app/Http/Controllers/Api/Raport/SiswaController.php
+++ app/Http/Controllers/Api/Raport/SiswaController.php
@@ -7,5 +7,30 @@ use Illuminate\Http\Request;
 
 class SiswaController extends Controller
 {
-    //
+    public function indexWeb() {
+        return view('raport.siswa');
+    }
+
+    public function create(Request $request){
+        $validatedReq = $request->validate([
+            'nama' => 'required|regex:/^[a-zA-Z\s]+$/u|max:255|min:3',
+            'kelas' => 'required|alpha',
+            'nik' => 'required|unique:r_siswa,nama',
+            'tempat_lahir' => 'required',
+            'tanggal_larir' => 'required|date_format:d-m-Y'
+        ]);
+        // save to table r_siswa
+        // redirect to page siswa
+        return redirect()->route('page.siswa');
+
+    }
+    public function getListSiswa(){
+        // get all data from table r_siswa where user_id 
+
+        return 'allsiswa data';
+    }
+
+    public function getSiswa(){
+        return 'return siswa with spesific id in userid';
+    }
 }
