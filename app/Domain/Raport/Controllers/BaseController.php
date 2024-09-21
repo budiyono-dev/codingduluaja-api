0a0510f25cc5df3c94026e1ef9ec7fb8256cfeb5
budiyono-dev <budiyono.dev@gmail.com>
2024-09-22 00:12:26 +0700

    [API-035] add dummy menu

--- app/Domain/Raport/Controllers/BaseController.php
+++ app/Domain/Raport/Controllers/BaseController.php
@@ -8,4 +8,75 @@ class BaseController
     {
         //
     }
+
+    protected function getMenu(string role){
+        $menus = [
+            [
+                'name'=>'Master Data'
+                'id'=> 1,
+                'order'=> 1,
+                'items'=>[
+                    [
+                        'id'=>1,
+                        'name'=>'Siswa',
+                        'route'=>'domain.raport.siswa.index',
+                    ],
+                    [
+                        'id'=>2,
+                        'name'=> 'Guru',
+                        'route'=> 'domain.raport.guru.index',
+                    ]
+                ]
+            ],
+            [
+                'name'=>'Nilai'
+                'id'=> 2,
+                'order'=> 2,
+                'items'=>[
+                    [
+                        'id'=>3,
+                        'name'=>'Ulangan',
+                        'route'=>'domain.raport.siswa.index',
+                    ],
+                    [
+                        'id'=>2,
+                        'name'=> 'UAS',
+                        'route'=> 'domain.raport.guru.index',
+                    ],
+                    [
+                        'id'=>2,
+                        'name'=> 'UTS',
+                        'route'=> 'domain.raport.guru.index',
+                    ],
+                    [
+                        'id'=>2,
+                        'name'=> 'Praktikum',
+                        'route'=> 'domain.raport.guru.index',
+                    ]
+                ]
+            ],
+            [
+                'name'=>'Raport'
+                'id'=> 3,
+                'order'=> 3,
+                'items'=>[
+                    [
+                        'id'=>3,
+                        'name'=>'Lihat Raport',
+                        'route'=>'domain.raport.siswa.index',
+                    ],
+                    [
+                        'id'=>2,
+                        'name'=> 'Generate Raport',
+                        'route'=> 'domain.raport.guru.index',
+                    ],
+                    [
+                        'id'=>2,
+                        'name'=> 'UTS',
+                        'route'=> 'domain.raport.guru.index',
+                    ]
+                ]
+            ]
+        ];
+    }
 }
